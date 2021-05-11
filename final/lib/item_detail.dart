f0ec7235657d7747c940441ad5cd4b965aebf057
jeonlego012 <jyo9873@gmail.com>
2021-05-11 23:41:06 +0900

    layout

--- final/lib/item_detail.dart
+++ final/lib/item_detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'login.dart';
 import 'model/product.dart';
 
 class ItemDetailPage extends StatefulWidget {
@@ -14,25 +16,33 @@ class ItemDetailPage extends StatefulWidget {
 class _ItemDetailPageState extends State<ItemDetailPage> {
   @override
   Widget build(BuildContext context) {
-    Widget imageSection = Container(
-      child: Image.asset(
-        widget.product.assetName,
-        package: widget.product.assetPackage,
-        width: 600,
-        height: 240,
-        fit: BoxFit.cover,
-      ),
-    );
-    Widget nameSection = Container(
-      padding: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 10.0),
-      child: Text(
-        widget.product.name,
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    );
+    // Widget imageSection = Container(
+    //   child: Image.asset(
+    //     widget.product.assetName,
+    //     package: widget.product.assetPackage,
+    //     width: 600,
+    //     height: 240,
+    //     fit: BoxFit.cover,
+    //   ),
+    // );
+    // Widget nameSection = Consumer<ApplicationState>(
+    //   builder: (context, appState, _) => Row(
+    //     children: <Widget>[
+    //       Column(
+    //         children: [
+
+    //         ],
+    //       ),
+    //     ],
+    //   ),
+    //   child: Text(
+    //     widget.product.name,
+    //     style: TextStyle(
+    //       fontSize: 20,
+    //       fontWeight: FontWeight.bold,
+    //     ),
+    //   ),
+    // );
     Widget iconSection = Container(
       padding: EdgeInsets.fromLTRB(20.0, 15.0, 0.0, 10.0),
       child: Column(
@@ -44,12 +54,33 @@ class _ItemDetailPageState extends State<ItemDetailPage> {
     );
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Detail'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.edit,
+              semanticLabel: 'edit',
+            ),
+            onPressed: () {
+              print('edit!');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.delete,
+              semanticLabel: 'delete',
+            ),
+            onPressed: () {
+              print('delete!');
+            },
+          ),
+        ],
       ),
       body: ListView(
         children: [
-          imageSection,
-          nameSection,
+          // imageSection,
+          // nameSection,
           _buildDivider(),
           iconSection,
         ],
